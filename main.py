963b10751c04634ea8f646419b162621c988b3e4
jesus perez <firemax366@gmail.com>
2020-10-24 14:50:04 +0200

    juego completatado

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ User_name=input("Ingresar su Nombre: ")
 print(f"Es momento de jugar {User_name}, espero que este preparado para el reto")
 print("Let`s Goooooo")
 print("ahora es tu turno de empezar adivinar la palabra")
-word_list=["maria", "jesus", "casa"]
-word=random.choice(word_list)
-print("_ "*len(word))
-letter=input("por favor ingresa una letra del alfabeto :").upper
-alg.Inp_Letter(letter,word)
+word_list=["marias", "jesus", "casa"]
+word=random.choice(word_list).upper()
+look="_ "*len(word)
+print(look)
+letter=input("por favor ingresa una letra del alfabeto :").upper()
+alg.Inp_Letter(letter,word,look)
--- src/algoritmo.py
+++ src/algoritmo.py
@@ -3,33 +3,187 @@ from string import ascii_uppercase
 ascii_uppercase += 'Ñ'
 alphab=[i for i in ascii_uppercase]
 alphab_not_use=alphab
-def Inp_Letter(letter,word):
-    letter=input("por favor ingresa una letra del alfabeto :").upper
-    if letter in alphab:
-        if letter in word:
-            if letter in alphab_not_use:
-                alphab_not_use.remove(letter)
-                for i in word:
-                    if i == letter:
-                        count+=1
-                        #hay que pensar como sustituir los _ por las letras
-                    elif count==len(word):
-                        print("Usted es el Ganador!!!!!!")
-                        return
-                Inp_Letter(input("por favor ingresa una letra del alfabeto :").upper,word)
-            else:
-                print("usted ya uso esta letra")
-                Inp_Letter(input("por favor ingresa una letra del alfabeto :").upper,word)
-        else:
-            print("la letra escojida no la tiene la palabra")
-            alphab_not_use.remove(letter)
-            
-            #inventarse lo del dibujo
-            
+count=0
+position=0
+a=1
+def look_alpha_not_use(letter, alphab_not_use,word,look):
+    global count
+    global position
+    if letter in alphab_not_use:
+        alphab_not_use.remove(letter)
+        position=0
+        for i in word:
+            position+=1
+            if i == letter:
+                count+=1
+                print(count)
+                look=look.split()
+                look[position-1] = i
+                look=" ".join(look)
+                print(look)
+
+                #hay que pensar como sustituir los _ por las letras
+        if count==len(word):
+            print('''
+_____.___.________   ____ ___      __      __.___ _______   
+\__  |   |\_____  \ |    |   \    /  \    /  \   |\      \  
+ /   |   | /   |   \|    |   /    \   \/\/   /   |/   |   \ 
+ \____   |/    |    \    |  /      \        /|   /    |    \ 
+ / ______|\_______  /______/        \__/\  / |___\____|__  /
+ \/               \/                     \/              \/
+
+            ''')
+            return
+        Inp_Letter(input("por favor ingresa una letra del alfabeto2 :").upper(),word,look)
+
+def look_word(letter,word,look):
+    if letter in word:
+        look_alpha_not_use(letter, alphab_not_use,word,look)
     else:
-        if False:
+        print("la letra escojida no la tiene la palabra")
+        alphab_not_use.remove(letter)
+        toy(a)
+        print("")
+        print(look)
+        if a == 9:
             print("Perdiste!!!!!!")
             return
+        Inp_Letter(input("por favor ingresa una letra del alfabeto4 :").upper(),word,look)
+        #inventarse lo del dibujo
+        
+
+def look_alphab (letter,alphab,word,look):
+    if letter in alphab:
+        look_word(letter,word,look)
+
+    else:
+        if letter > "20" :
+            print("la letra escojida usted ya la uso")
+            print(look)
+            Inp_Letter(input("por favor ingresa una letra del alfabeto5 :").upper(),word,look)
         else:
-            Inp_Letter(input("por favor ingresa una letra del alfabeto :").upper,word)
+            print("usted ha ingresado un numero o un caracter")
+            print(look)
+            Inp_Letter(input("por favor ingresa una letra del alfabeto5 :").upper(),word,look)
+
+
+def Inp_Letter(letter,word,look):
+    if len(letter) == 1:
+        look_alphab(letter,alphab,word,look)
+    else:
+            print("ingreso mas de una letra")
+            print(look)
+            Inp_Letter(input("por favor ingresa una letra del alfabeto5 :").upper(),word,look)
+
+
+def toy(b):
+    print(b)
+    global a
+    if b == 1:
+        a=2
+        print('''     
+    ________________________
+    ''')
+        print(a)
+    elif b == 2:
+        a=3
+        print(''' 
+                |
+                |
+                |
+                |
+                |
+                | 
+    ____________|____________
+    ''')
+                    
+    elif b == 3:
+        a=4
+        print('''   
+                _____________
+                |
+                |
+                |
+                |
+                |
+                | 
+    ____________|____________
+    ''')
+
+    elif b == 4:
+        a=5
+        print('''   
+                _____________
+                |            O
+                |
+                |
+                |
+                |
+                | 
+    ____________|____________
+    ''')
+
+    elif b == 5:
+        a=6
+        print('''   
+                _____________
+                |            O
+                |            |
+                |            |
+                |
+                |
+                | 
+    ____________|____________
+    ''')
+
+    elif b == 6:
+        a=7
+        print('''   
+                _____________
+                |            O
+                |           /|
+                |            |
+                |           /
+                |
+                | 
+    ____________|____________
+    ''')
+
+    elif b == 7:
+        a=8
+        print('''
+                _____________
+                |            O
+                |           /|/
+                |            |
+                |           / /
+                |
+                | 
+    ____________|____________
+    ''')
+
+    elif b == 8:
+        a=9
+        print('''   
+                _______________
+                |          |_O_|
+                |           /|/
+                |            |
+                |           / /
+                |
+                | 
+    ____________|____________
+    ''')
+        print('''  
+
+  ________    _____      _____  ___________ ____________   _________________________ 
+ /  _____/   /  _  \    /     \ \_   _____/ \_____  \   \ /   /\_   _____/\______   \ 
+/   \  ___  /  /_\  \  /  \ /  \ |    __)_   /   |   \   Y   /  |    __)_  |       _/
+\    \_\  \/    |    \/    Y    \|        \ /    |    \     /   |        \ |    |   \ 
+ \______  /\____|__  /\____|__  /_______  / \_______  /\___/   /_______  / |____|_  /
+        \/         \/         \/        \/          \/                 \/         \/ 
+        ''')
+
+
+
 
